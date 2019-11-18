807cb720019f2da04bc8cf0ff224c7aec970ee8a
Thomas Burleson <ThomasBurleson@Gmail.com>
2019-11-18 17:23:22 -0600

    feat(di): add support for parent injectors
    Feature to allow chain of injectors to be link.
    *  Injectors can search parent providers.
    *  Child injectors can override provider entries.

--- lib/utils/src/lib/di/injector.spec.ts
+++ lib/utils/src/lib/di/injector.spec.ts
@@ -3,18 +3,87 @@ import { DependencyInjector, UndoChanges } from './injector.interfaces';
 import { makeInjector } from './injector';
 
 describe('DependencyInjector', () => {
-  const MSG_TOKEN = new InjectionToken("injector.spec.ts - msg");
+  let parent: DependencyInjector;
   let injector: DependencyInjector;
+  let MSG_TOKEN: InjectionToken<string>;
 
   describe('with useClass', () => {
     beforeEach(() => {
-      injector = makeInjector([
-        { provide: MSG_TOKEN, useValue: "Hello Thomas"},
-        { provide: A, useClass: A, deps:[MSG_TOKEN]},
-        { provide: B, useClass: B, deps:[A]},
-        { provide: C, useClass: C, deps:[A]},
-        { provide: D, useClass: D, deps:[B, C]},
+      const [injectorA, tokenA] = makeTestInjector();
+
+      injector = injectorA;
+      MSG_TOKEN = tokenA;
+    })
+
+    it('should inject A which has InjectionToken dependencies', () => {
+      const instA: A = injector.get(A);
+
+      expect(instA.title).toBe("A");
+      expect(instA.msg).toBe("Hello Thomas");
+    });
+
+    it('should inject B which has A dependencies', () => {
+      const instB: B = injector.get(B);
+
+      expect(instB.title).toBe("B");
+      expect(instB.a.title).toBe("A");
+    });
+
+    it('should inject D which has B + C dependencies', () => {
+      const instD: D = injector.get(D);
+
+      expect(instD.title).toBe("D");
+      expect(instD.b.title).toBe("B");
+      expect(instD.c.title).toBe("C");
+    });    
+
+    it('should support mock injection B', () => {
+      injector.addProviders([ {provide: B, useClass: MockB } ]); 
+
+      const instB: B = injector.get(B);
+      expect(instB.title).toBe("MockB");
+      expect(instB.a.title).toBe("MockA");
+
+      const instD: D = injector.get(D);
+      expect(instD.b.title).toBe("MockB");
+    });    
+
+    it('should allow A deps overrides with useFactory', () => {
+      injector.addProviders([ { provide: MSG_TOKEN, useFactory: () => "windy" } ])
+      const instA: A = injector.get(A);
+
+      expect(instA.title).toBe("A");
+      expect(instA.msg).toBe("windy");
+    });   
+
+    it('should undo changes after addProviders()', () => {
+      const undoChanges: UndoChanges = injector.addProviders([ 
+        { provide: MSG_TOKEN, useFactory: () => "windy" } 
       ]);
+      
+      let instA: A = injector.get(A);
+      expect(instA.title).toBe("A");
+      expect(instA.msg).toBe("windy");
+
+      undoChanges();
+      instA = injector.get(A);
+      
+      expect(instA.msg).toBe("Hello Thomas");
+    });   
+
+  });
+
+  describe('with parent injector', () => {
+    beforeEach(() => {
+      const [parentA, tokenA] = makeTestInjector();
+
+      parent = parentA;
+      injector = makeInjector([
+        { provide: E, useClass: E, deps:[D] },
+        { provide: F, useClass: F, deps:[E, A] },
+      ], parent);
+      
+      MSG_TOKEN = tokenA;
     })
 
     it('should inject A which has InjectionToken dependencies', () => {
@@ -59,7 +128,7 @@ describe('DependencyInjector', () => {
     });   
 
     it('should undo changes after addProviders()', () => {
-      const undoChanges: UndoChanges = injector.addProviders([ 
+      const undoChanges: UndoChanges = parent.addProviders([ 
         { provide: MSG_TOKEN, useFactory: () => "windy" } 
       ]);
       
@@ -68,19 +137,36 @@ describe('DependencyInjector', () => {
       expect(instA.msg).toBe("windy");
 
       undoChanges();
+      injector.reset();
+
       instA = injector.get(A);
       
       expect(instA.msg).toBe("Hello Thomas");
     });   
 
   });
-
 });
 
 
+function makeTestInjector(): [ DependencyInjector, InjectionToken<string> ] {
+  const token = new InjectionToken("injector.spec.ts - msg");
+  const injector = makeInjector([
+    { provide: token, useValue: "Hello Thomas"},
+    { provide: A, useClass: A, deps:[token]},
+    { provide: B, useClass: B, deps:[A]},
+    { provide: C, useClass: C, deps:[A]},
+    { provide: D, useClass: D, deps:[B, C]},
+  ]);
+  return [injector, token];
+}
 
 class A { constructor(public msg: string, public title = "A"){} }
 class B { constructor(public a: A, public title="B"){ }}
 class C { constructor(public a: A, public title="C"){ }}
 class D { constructor(public b: B, public c: C, public title="D"){ }}
-class MockB {  a = { title: "MockA" }; title = "MockB"; }
+class E { constructor(public d: D, public title="E"){ }}
+class F { constructor(public e: E, public a:A){ }}
+class MockB { 
+  a = { title: "MockA" }; 
+  constructor(public msg: string, public title = "MockB"){} 
+}
--- lib/utils/src/lib/di/injector.ts
+++ lib/utils/src/lib/di/injector.ts
@@ -1,5 +1,8 @@
-import { DependencyInjector, UndoChanges } from './injector.interfaces';
-import { Provider, TypeProvider, makeClassProvider } from './injector.interfaces';
+import { 
+  DependencyInjector, UndoChanges, Token,
+  Provider, TypeProvider 
+} from './injector.interfaces';
+
 
 /**
  * Utility function used to easily create 1..n injectors; each with thier
@@ -8,13 +11,13 @@ import { Provider, TypeProvider, makeClassProvider } from './injector.interfaces
  * NOTE: If only a class is registered (instead of a Provider), convert to it 
  * for normalized usages
  */
-export function makeInjector(registry: (Provider | TypeProvider)[]): DependencyInjector {
+export function makeInjector(registry: (Provider | TypeProvider)[], parent?: DependencyInjector): DependencyInjector {
   const normalized = registry.map(it => {
     const isProvider = !!(it as Provider).provide;
     return isProvider ? it : makeClassProvider(it);
   }) as Provider[];
 
-  return new Injector(normalized);
+  return new Injector(normalized, parent);
 }
 
 /**
@@ -24,33 +27,39 @@ export function makeInjector(registry: (Provider | TypeProvider)[]): DependencyI
 class Injector implements DependencyInjector {
   private singletons = new WeakMap();
 
-  constructor(private providers: Provider[] = []) {
+  constructor(private providers: Provider[] = [], private parent?: DependencyInjector) {
     this.addProviders(providers);
   }
 
   /**
    * Lookup singleton instance using token
    * Optionally create instance and save as singleton if needed
+   * If not found, this will search a parent injector (if provided)
    */
-  get(token: any): any {
-    return this.findAndMakeInstance(token);
+  get(token: Token): any {
+    var inst = this.findAndMakeInstance(token);
+    return inst || (this.parent ? this.parent.get(token) : null);
   }
 
+
+  /**
+   * Force-clear internally singleton cache. 
+   * If parents are specified, this enforces correct parent lookups
+   * for future instantiations.
+   */
+  reset(): void {
+    if (this.parent) {
+      this.singletons = new WeakMap();
+    }
+  }
   /**
    * Create an unshared, non-cached instance of the token;
    * based on the Provider configuration
+   * If not found, then consider asking parent injector for the
+   * instance
    */
-  instanceOf(token: any): any {
-    const provider = this.findLastRegistration(token, this.providers);
-    const deps = provider && provider.deps ? provider.deps.map(it => this.instanceOf(it)) : [];
-    const makeWithClazz = (clazz: any) => (clazz ? new clazz(...deps) : null);
-    const makeWithFactory = (fn: () => any) => (fn ? fn.call(null, deps) : null);
-
-    return provider && ( provider.useValue
-      || makeWithClazz(provider.useClass) 
-      || makeWithFactory(provider.useFactory)
-      || makeWithClazz(provider.provide)  // fallback uses the token as a `class`
-    );
+  instanceOf(token: Token): any {
+    return this.instanceFromRegistry(token) || this.instanceFromParents(token);
   }
 
   /**
@@ -60,30 +69,51 @@ class Injector implements DependencyInjector {
    * @param registry Configuration set of Provider(s)
    * @param replace Replace existing provider
    */
-  addProviders(registry: Provider[], replace = true): UndoChanges {
+  addProviders(registry: Provider[]): UndoChanges {
     const origProviders = [...this.providers];    
-    const cache = replace
-      ? this.providers.reduce((list, current) => {
-          const isSameToken = newItem => newItem.provide === current.provide;
-          const notFound = registry.filter(isSameToken).length < 1;
-          return notFound ? list.concat([current]) : list;
-        }, [])
-      : this.providers;
-
-    this.providers = cache.concat(registry);
+    this.providers = mergeProviders(this.providers, registry);
     registry.map(it => this.singletons.delete(it.provide));
 
     return () => this.addProviders(origProviders);
   }
 
+
+  /**
+   * Get all Token providers from parent(s), updated with current overwrites (if any).
+   * 
+   * This allows the full parent registry tree to be flattened 
+   * with current level acting as overwrites
+   */
+  getFlatProviderTree(): Provider[] {
+    return !this.parent ? [...this.providers] : [
+      ...mergeProviders(this.parent.getFlatProviderTree(), this.providers)
+    ];
+  }
+  
   // *************************************************
   // Private  Methods
   // *************************************************
 
   /**
+   * Ask parent injectors (if any) for a flattened Provider registry,
+   * then try to build the instance. When done, restore the original 
+   * Provider registry for this injector 
+   */
+  private instanceFromParents(token: Token): any {
+    if( !this.parent ) return undefined;
+
+    const original = [...this.providers];
+    try {
+      this.providers = mergeProviders(this.getFlatProviderTree(), this.providers);
+      return this.instanceFromRegistry(token);
+    } finally {
+      this.providers = original;
+    }
+  }
+  /**
    * Find last Provider registration (last one wins)
    */
-  private findLastRegistration(token: any, list: Provider[]) {
+  private findLastRegistration(token: Token, list: Provider[]) {
     const registry = this.providers.filter(it => it.provide === token);
     return registry.length ? registry[registry.length - 1] : null;
   }
@@ -91,11 +121,69 @@ class Injector implements DependencyInjector {
   /**
    * Based on provider registration, create instance of token and save
    * as singleton value.
+   * NOTE: do not scan parent since we are caching singletons at this level only.
+   * 
    * @param token Class, value, or factory
    */
-  private findAndMakeInstance(token: any): any {
+  private findAndMakeInstance(token: Token): any {
     let result = this.singletons.get(token) || this.instanceOf(token);
-    this.singletons.set(token, result);
+    result && this.singletons.set(token, result);
+
     return result;
   }
+
+  private instanceFromRegistry(token:Token): any {
+    const provider = this.findLastRegistration(token, this.providers);
+    const deps = provider && provider.deps ? provider.deps.map(it => this.instanceOf(it)) : [];
+    const makeWithClazz = (clazz: any) => (clazz ? new clazz(...deps) : null);
+    const makeWithFactory = (fn: () => any) => (fn ? fn.call(null, deps) : null);
+
+    return provider && ( provider.useValue
+      || makeWithClazz(provider.useClass) 
+      || makeWithFactory(provider.useFactory)
+      || makeWithClazz(provider.provide)  // fallback uses the token as a `class`
+    );    
+  }
+
+
 }
+
+
+/**
+ * Internal utility used to normalized Provider entries
+ * during a `makeInjector()` call
+ * 
+ * @param token
+ */
+function makeClassProvider(token:any): Provider {
+  return {
+    provide: token,
+    useClass: token,
+    deps: [...token['deps']],
+  };
+}
+
+
+  /**
+   * Merge current provider registry with 1..n updated Provider settings;
+   * where the `updated` set may contain either modified and/or new configurations
+   */
+  function mergeProviders(current: Provider[], updated: Provider[]): Provider[] {
+    // Overwrite existing cache entry if an update is provided
+    const cache = current.reduce((list, entry) => {
+      const lastItem = (list) => list[list.length - 1];
+      const overwrites = updated.filter(it => it.provide === entry.provide);
+      const found = !!overwrites.length;
+
+      list.push(found ? lastItem(overwrites) : entry);
+      return list;
+    }, []);
+
+    // If not overwritten (in the cache), add it as a new entry.
+    updated.map(it => {
+      const isNew  = cache.indexOf(it) < 0;
+      isNew && cache.push(it);
+    });
+
+    return cache;
+  };
